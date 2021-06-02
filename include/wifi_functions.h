d043a00bc80edc696f8f74851bfe9b0ec9205254
Cyrus <cyrus.cheung123@gmail.com>
2021-06-01 22:11:25 -0700

    tidying code

---
+++ include/wifi_functions.h
@@ -0,0 +1,91 @@
+#include <SPI.h>
+#include <WiFiNINA.h>
+
+#include "arduino_secrets.h" 
+
+#define SERVER "192.168.1.106"
+#define PORT 5000
+#define CONTENT_TYPE "Content-type: application/x-www-form-urlencoded"
+
+WiFiClient client;
+
+class diagnostic{
+    public:
+        void checkWifiStatus();
+        void printWifiStatus();
+        void checkWifiVersion();
+} diag;
+
+void diagnostic::checkWifiStatus(){
+    if (WiFi.status() == WL_NO_MODULE) {
+        Serial.println("Communication with WiFi module failed!");
+        // don't continue
+         //diag.checkWifiStatus();
+    }
+}
+
+void diagnostic::printWifiStatus(){
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
+
+    // print your board's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.print(rssi);
+    Serial.println(" dBm");
+} 
+
+void diagnostic::checkWifiVersion(){
+    String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+}
+
+void wifiConnect(){
+    int status = WL_IDLE_STATUS;
+     // attempt to connect to WiFi network:
+    while (status != WL_CONNECTED) {
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+        status = WiFi.begin(SECRET_SSID, SECRET_PASS);
+    }
+}
+
+void postRequest(String postData){
+
+    if (client.connect(SERVER, PORT)) {
+    
+    String dataLength = String("Content-Length: ") + postData.length() + "\r\n";
+    // Make a HTTP request:
+    client.println("POST / HTTP/1.1");
+    client.println("Host: 192.168.1.106");    
+    client.println("Connection: close");
+
+    client.println(CONTENT_TYPE);
+    client.println(dataLength);
+    client.print(postData);
+  }
+  else{
+
+  } 
+}
+
+void displayResponse(){
+
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  
+  //Serial.println("=================================");
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
+  //Serial.println("\n=================================");
+  
+}
