2669a97dfd3da1b5bfe505f4c0254f7e29448e7f
Cyrus <cyrus.cheung123@gmail.com>
2021-06-19 23:41:15 -0700

    added 3 tasks, and IPC queues

--- src/asd.cpp
+++ src/asd.cpp
@@ -6,79 +6,84 @@
 #include "mcu_setup.cpp"
 #include "initialize.cpp"
 
+#define READ_ANALOG_DELAY_MS 100
+#define PROCESS_DATA_DELAY 100
+#define UPLOAD_DATA_DELAY 50
 
-#define READ_ANALOG_DELAY_MS 
-#define CHECK_WIFI_DELAY_MS 5000
+QueueHandle_t Global_Queue_Handle = 0;
+QueueHandle_t Message_Queue = 0;
 
 void readAnalogStick(void* parameters){
-  int x1 = 0;
-  int y1 = 0;
-  int count = 0;
-  String data;
+  int values[2];
+
   while(true){
+    values[0] = (analogRead(A1)*100)/1023;
+    values[1] = (analogRead(A2)*100)/1023;
+    xQueueSend(Global_Queue_Handle, &values, 1000);
 
-    x1 += (analogRead(A1)*100)/1023;
-    //int y1 = (analogRead(A2));//*100)/1023;
-    //int x2 = (analogRead(A1));//*100)/1023;
-    y1 += (analogRead(A2)*100)/1023;
-    count++;
-    if (count > 3){
-
-
-        x1 = (int)x1/4;
-        y1 = (int)y1/4;
-        data = ((String("x1=")) + x1 + "&" +"y1=" + y1);
-
-        // Serial.print(x1);
-        // Serial.print("% ");
-        // Serial.print(y1);
-        // Serial.println("% ");
-        //Serial.println(data);
-        Serial.print("Posting Data");
-        if (!diag.isWifiConnected()){
-          wifiConnect();
-        }
-        postRequest(data);
-        x1 = y1 = 0;
-        count = 0;
-        
-
-    }
-
-    vTaskDelay(250 / portTICK_PERIOD_MS);
+    vTaskDelay(READ_ANALOG_DELAY_MS / portTICK_PERIOD_MS);
   }
 }
 
-void wifiCheck(void* parameters){
+void processData(void* parameters){
+  String data;
+  int rx_value[2];
   while(true){
-
-    if(diag.isWifiConnected()){
-      Serial.println("Wifi Still here:)");
-      vTaskDelay(1000/portTICK_PERIOD_MS);
-      continue;
-    }
-    Serial.println("Suspended Tasks");
     
-    Serial.println("Resumed Tasks");
-      
+    xQueueReceive(Global_Queue_Handle, &rx_value, 500);
+    
+    data = ((String("x1=")) + rx_value[0] + "&" +"y1=" + rx_value[1]);
+
+    xQueueSend(Message_Queue, &data, 500);
+    
+    vTaskDelay(PROCESS_DATA_DELAY / portTICK_PERIOD_MS);
+  }
+}
+
+void uploadData(void* parameters){
+  String data = "Initial String";
+
+  while(true){
+    
+    if(!diag.isWifiConnected()){
+      wifiConnect();
+    }
+    xQueueReceive(Message_Queue, &data, 500);
+    Serial.println(data);
+    postRequest(data);
+
+
+    vTaskDelay( UPLOAD_DATA_DELAY/portTICK_PERIOD_MS);  
   }
 }
 
 void FreeRTOS_INITIALIZE(){
 
+  Global_Queue_Handle = xQueueCreate(5, 2*sizeof(int));
+  Message_Queue = xQueueCreate(5, 20*sizeof(char));
+
   xTaskCreate(
         readAnalogStick, // Function to be called
         "Read Analog",   // Name of task
-        1024,            // Size of stack
+        512,            // Size of stack
         NULL,            // Parameters for function
         1,               // Task Priority
         NULL             // Task Handle (pointer)
     );
 
     xTaskCreate(
-        wifiCheck, // Function to be called
-        "Wifi Check",   // Name of task
-        1024,            // Size of stack
+        uploadData,      // Function to be called
+        "Uploading Data",   // Name of task
+        512,            // Size of stack
+        NULL,            // Parameters for function
+        3,               // Task Priority
+        NULL             // Task Handle (pointer)
+    );
+
+    xTaskCreate(
+        processData, // Function to be called
+        "Data Process",   // Name of task
+        512,            // Size of stacOHk
         NULL,            // Parameters for function
         2,               // Task Priority
         NULL             // Task Handle (pointer)
